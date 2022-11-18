e0848ea1639e06ffeec47779ef92bf823869907d
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2022-11-18 12:13:23 +0330

    testing modals

--- Login.js
+++ Login.js
@@ -5,3 +5,10 @@ jQuery(document).ready(function($) {
 })
 
 // JavaScript
+// Error modal close button
+const error_btn = document.querySelector('.error_btn');
+const error_container = document.querySelector('.error_container');
+
+error_btn.addEventListener('click', () => {
+    error_container.style.display = 'none';
+});
