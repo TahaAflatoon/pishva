e030932668c3f2b8f03e38dc6c2dd77812147392
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2022-11-19 13:45:59 +0330

    some little changes

--- Login.js
+++ Login.js
@@ -12,3 +12,21 @@ const error_container = document.querySelector('.error_container');
 error_btn.addEventListener('click', () => {
     error_container.style.display = 'none';
 });
+
+// Inputs eye toggle
+username_toggle = () => {
+    let pass = document.getElementById('username');
+    if (pass.type === 'password') {
+        pass.type = 'text';
+    } else {
+        pass.type = 'password';
+    }
+}
+password_toggle = () => {
+    let pass = document.getElementById('psw');
+    if (pass.type === 'password') {
+        pass.type = 'text';
+    } else {
+        pass.type = 'password';
+    }
+}
